6ca74f058da5e563b0db6eb4ec4613605d12e843
cazyan <caz.yang@softwire.com>
2025-10-28 17:11:27 +0000

    feat: added "drawGame" after keypress so immediate feedback on movement, rotation etc.

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -32,29 +32,37 @@ function updateGame() {
 function setupControls() {
     document.addEventListener('keydown', (event) => {
         switch(event.key) {
+            
             case 'ArrowLeft':
                 game.moveLeft();
+                drawGame(game);
                 break;
             case 'ArrowRight':
                 game.moveRight();
+                drawGame(game);
                 break;
             case 'ArrowUp':
                 game.rotateTetrominoClockwise();
+                drawGame(game);
                 break;
             case 'ArrowDown':
                 // TODO: implement soft drop
                 
             case ' ': // Space
                 game.instantDropTetromino();
+                drawGame(game);
                 break;
             case 'c':
             case 'C':
                 game.holdCurrentTetromino();
+                drawGame(game);
                 break;
             case 'z':
             case 'Z':
                 game.rotateTetrominoAntiClockwise();
+                drawGame(game);
                 break;
+            
         }
         drawGameState();
     });
