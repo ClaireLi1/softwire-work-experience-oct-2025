423e35d573c832d0c10afe0c10a5f43d2dd07e33
cazyan <caz.yang@softwire.com>
2025-10-28 09:52:46 +0000

    NEW MVP for students to get started: added EventListener and tidied up structure

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -4,23 +4,57 @@ import { drawGame, drawGrid } from "./gameUI.js"
 const GAME_TICK_INTERVAL_MS = 1000;
 
 let game = initialiseGame();
-gameLoop();
 
 function initialiseGame() {
     let game = createGame();
 
     drawGrid();
-    drawGame(game);
+    setupControls();
 
     console.log(game);
+
+    // Start the game loop
+    setInterval(() => {
+        updateGame();
+        
+    }, GAME_TICK_INTERVAL_MS);
 
     return game;
 }
 
-function gameLoop() {
-    setInterval(() => {
-        game.gameTick();
-        drawGame(game);
-        
-    }, GAME_TICK_INTERVAL_MS);    
+function updateGame() {
+    game.gameTick();
+    drawGame(game);
+
+    // other things that should happen each game tick to be added here
+}
+
+function setupControls() {
+    document.addEventListener('keydown', (event) => {
+        switch(event.key) {
+            case 'ArrowLeft':
+                game.moveLeft();
+                break;
+            case 'ArrowRight':
+                game.moveRight();
+                break;
+            case 'ArrowUp':
+                game.rotateTetrominoClockwise();
+                break;
+            case 'ArrowDown':
+                // TODO: implement soft drop
+            case ' ': // Space
+                game.instantDropTetromino();
+                break;
+            case 'c':
+            case 'C':
+                game.holdCurrentTetromino();
+                break;
+            case 'z':
+            case 'Z':
+                game.rotateTetrominoAntiClockwise();
+                break;
+        }
+        drawGameState();
+    });
 }
