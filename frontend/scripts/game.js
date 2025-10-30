44c666f228461ebc35d2056826eed5423489483b
Carolyn <carolyn.barker@softwire.com>
2025-10-30 20:26:47 +0000

    poll for and handle gamepad input

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -23,6 +23,9 @@ function initialiseGame() {
 }
 
 function setupControls() {
+    // we don't get events when a gamepad button is pushed, so we have to poll for it
+    setInterval(pollForGamepadInput, 50)
+
     document.addEventListener('keydown', (event) => {
         switch(event.key) {
             
@@ -101,4 +104,43 @@ function setupControls() {
             /* End debug controls */
         }
     });
+
+    function pollForGamepadInput() {
+        // the gamepad list is regenerated every time a button is pressed, so we need to get them every "frame"
+        var gamepads = navigator.getGamepads().filter(x=>x!=null)
+
+        if (gamepads.length > 0) {
+            var leftInputs = gamepads.filter(gp => gp.axes[0] == -1)
+            var rightInputs = gamepads.filter(gp => gp.axes[0] == 1)
+            var upInputs = gamepads.filter(gp => gp.axes[1] == -1)
+            var downInputs = gamepads.filter(gp => gp.axes[1] == 1)
+
+            if (leftInputs.length > 0 & rightInputs.length == 0 ) {
+                game.moveLeft();
+            }
+            else if (rightInputs.length > 0 & leftInputs.length == 0) {
+                game.moveRight();
+            }
+            else if (upInputs.length > 0 & downInputs.length == 0) {
+                game.rotateTetrominoClockwise();
+            }
+            else if (downInputs.length > 0 & upInputs.length == 0) {
+                game.moveDown();
+            }
+            // the buttons seem to just be identified by their order in the array
+            // on the specific controller we've got at Softwire, the first few buttons are A, B, X, Y
+            else if (gamepads.some(x=>x.buttons[0].pressed)) { // A - hard drop
+                game.instantDropTetromino();
+            }
+            else if (gamepads.some(x=>x.buttons[1].pressed)) { // B - hold
+                game.holdCurrentTetromino();
+            }
+
+            if (gamepads.some(x=>x.buttons.slice(2).some(x=>x.pressed))) {
+                console.log("some button(s) were pressed on gamepad with no effect")
+            }
+
+            drawGame(game);
+        }
+    }
 }
