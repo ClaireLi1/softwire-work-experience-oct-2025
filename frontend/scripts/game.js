6d1c3b9984a0e99e56ae2b96c6df6f680af70766
Carolyn <carolyn.barker@softwire.com>
2025-10-31 12:53:20 +0000

    rebind keys and debounce rotation

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -104,43 +104,68 @@ function setupControls() {
             /* End debug controls */
         }
     });
+}
 
-    function pollForGamepadInput() {
-        // the gamepad list is regenerated every time a button is pressed, so we need to get them every "frame"
-        var gamepads = navigator.getGamepads().filter(x=>x!=null)
+// when you rotate by keyboard, your operating system uses a repeat delay
+// so if you tap a key, it is sent once per tap, and if you hold it down it repeats after a while
+// on controller it repeats immediately which is fine for joystick but unfun for buttons
+// this is a workaround for that, to prevent re-rotating too quickly etc
+let lastButtonPressTimes = [Date.now(), Date.now(), Date.now(), Date.now()];
+const buttonRepeatDelay = 200;
 
-        if (gamepads.length > 0) {
-            var leftInputs = gamepads.filter(gp => gp.axes[0] == -1)
-            var rightInputs = gamepads.filter(gp => gp.axes[0] == 1)
-            var upInputs = gamepads.filter(gp => gp.axes[1] == -1)
-            var downInputs = gamepads.filter(gp => gp.axes[1] == 1)
+function pollForGamepadInput() {
+    // the gamepad list is regenerated every time a button is pressed, so we need to get them every "frame"
+    var gamepads = navigator.getGamepads().filter(x=>x!=null)
 
-            if (leftInputs.length > 0 & rightInputs.length == 0 ) {
-                game.moveLeft();
+    if (gamepads.length > 0) {
+        var leftInputs = gamepads.filter(gp => gp.axes[0] == -1)
+        var rightInputs = gamepads.filter(gp => gp.axes[0] == 1)
+        var upInputs = gamepads.filter(gp => gp.axes[1] == -1)
+        var downInputs = gamepads.filter(gp => gp.axes[1] == 1)
+
+        if (leftInputs.length > 0 & rightInputs.length == 0 ) {
+            game.moveLeft();
+        }
+        else if (rightInputs.length > 0 & leftInputs.length == 0) {
+            game.moveRight();
+        }
+        else if (downInputs.length > 0 & upInputs.length == 0) {
+            game.moveDown();
+        }
+        // the buttons seem to just be identified by their order in the array
+        // on the specific controller we've got at Softwire, the first few buttons are A, B, X, Y
+        else if (gamepads.some(x=>x.buttons[0].pressed)) { // A - turn anticlockwise
+            var inputTime = Date.now();
+            if (inputTime - lastButtonPressTimes[0] > buttonRepeatDelay) {
+                lastButtonPressTimes[0] = inputTime;
+                game.rotateTetrominoAntiClockwise();
             }
-            else if (rightInputs.length > 0 & leftInputs.length == 0) {
-                game.moveRight();
-            }
-            else if (upInputs.length > 0 & downInputs.length == 0) {
+        }
+        else if (gamepads.some(x=>x.buttons[1].pressed)) { // B - turn clockwise
+            var inputTime = Date.now();
+            if (inputTime - lastButtonPressTimes[1] > buttonRepeatDelay) {
+                lastButtonPressTimes[1] = inputTime;
                 game.rotateTetrominoClockwise();
             }
-            else if (downInputs.length > 0 & upInputs.length == 0) {
-                game.moveDown();
-            }
-            // the buttons seem to just be identified by their order in the array
-            // on the specific controller we've got at Softwire, the first few buttons are A, B, X, Y
-            else if (gamepads.some(x=>x.buttons[0].pressed)) { // A - hard drop
+        }
+        else if (gamepads.some(x=>x.buttons[2].pressed)|| gamepads.some(x=>x.buttons[7].pressed)) { // X or RT - drop
+            var inputTime = Date.now();
+            if (inputTime - lastButtonPressTimes[2] > buttonRepeatDelay) {
+                lastButtonPressTimes[2] = inputTime;
                 game.instantDropTetromino();
             }
-            else if (gamepads.some(x=>x.buttons[1].pressed)) { // B - hold
+        }
+        else if (gamepads.some(x=>x.buttons[3].pressed)|| gamepads.some(x=>x.buttons[6].pressed)) { // Y or LT - hold
+            var inputTime = Date.now();
+            if (inputTime - lastButtonPressTimes[3] > buttonRepeatDelay) {
+                lastButtonPressTimes[3] = inputTime;
                 game.holdCurrentTetromino();
             }
-
-            if (gamepads.some(x=>x.buttons.slice(2).some(x=>x.pressed))) {
-                console.log("some button(s) were pressed on gamepad with no effect")
-            }
-
-            drawGame(game);
         }
+        else if (gamepads.some(x=>x.buttons.some(x=>x.pressed))) {
+            console.log("some button(s) were pressed on gamepad with no effect")
+        }
+
+        drawGame(game);
     }
 }
